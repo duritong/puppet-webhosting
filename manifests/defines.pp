1ca78b70e20525709d98d2dcde39105f88f6919e
mh <mh@immerda.ch>
2008-11-07 21:25:44 +0000

    added basic nagios checks

--- manifests/defines.pp
+++ manifests/defines.pp
@@ -11,7 +11,9 @@ define webhosting::static(
     $ssl_mode = 'false',
     $vhost_mode = 'template',
     $vhost_source = 'absent',
-    $vhost_destination = 'absent'
+    $vhost_destination = 'absent',
+    $nagios_check_url = '/',
+    $nagios_check_code = 'OK'
 ){
     user::sftp_only{"${name}":
         password => $password,
@@ -28,5 +30,13 @@ define webhosting::static(
         vhost_mode => $vhost_mode,    
         vhost_source => $vhost_source,
         vhost_destination => $vhost_destination,
+    }
+
+    if $use_nagios {
+        nagios::service::http{"${name}":
+            ssl_mode => $ssl_mode,
+            check_url => $nagios_check_url,
+            check_code => $nagios_check_code, 
+        }
     }
 }
