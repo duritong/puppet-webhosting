ddc4ad87db67a3d9020e4036eff5f14169a365e9
mh <mh@immerda.ch>
2008-12-01 23:25:15 +0000

    introduced itk stuff, adjusted naming

--- manifests/defines.pp
+++ manifests/defines.pp
@@ -70,7 +70,13 @@ define webhosting::static(
     }
 }
 
-define webhosting::perl(
+# run_mode: 
+#   - normal: nothing special (*default*)
+#   - itk: apache is running with the itk module 
+#          and run_uid and run_gid are used as vhost users
+# run_uid: the uid the vhost should run as with the itk module
+# run_gid: the gid the vhost should run as with the itk module
+define webhosting::modperl(
     $uid = 'absent',
     $gid = 'uid',
     $password = 'absent',
@@ -78,6 +84,9 @@ define webhosting::perl(
     $domainalias = 'www',
     $owner = root,
     $group = 'sftponly',
+    $run_mode = 'normal',
+    $run_uid = 'absent',
+    $run_gid = 'absent',
     $allow_override = 'None',
     $options = 'absent',
     $additional_options = 'absent',
@@ -101,13 +110,37 @@ define webhosting::perl(
         password_crypted => $password_crypted,
     }
 
-    apache::vhost::perl{"${name}":
+   case $run_mode {
+        'itk': {
+            case $run_uid {
+                'absent': { fail("you need to define run_uid for $name on $fqdn to use itk") }
+            }
+            case $run_gid {
+                'absent': {
+                    case $gid {
+                        'uid': { $real_run_gid = $uid }
+                        default: { $real_run_gid = $gid }
+                    }
+                }
+                default: { $real_run_gid = $run_gid }
+            }
+            user::managed{"${name}_run":
+                uid => $run_uid,
+                gid => $real_run_gid,
+            }
+        }
+    } 
+
+    apache::vhost::modperl{"${name}":
         domainalias => $real_domainalias,
         group => $group,
         documentroot_owner => $name,
         allow_override => $allow_override,
         options => $options,
         additional_options => $additional_options,
+        run_mode => $run_mode,
+        run_uid => $run_uid,
+        run_gid => $real_run_gid,
         ssl_mode => $ssl_mode,
         vhost_mode => $vhost_mode,
         vhost_source => $vhost_source,
