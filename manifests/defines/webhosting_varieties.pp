910906b9205776d27d4c4c10eb6cc821ce2e414f
mh <mh@immerda.ch>
2008-12-07 15:10:53 +0000

    fixed www alias problem, added a first version for php stuff

--- manifests/defines/webhosting_varieties.pp
+++ manifests/defines/webhosting_varieties.pp
@@ -41,7 +41,7 @@ define webhosting::static(
         nagios_check_code => $nagios_check_code,
     }
     apache::vhost::static{"${name}":
-        domainalias => $real_domainalias,
+        domainalias => $domainalias,
         group => $group,
         documentroot_owner => $name, 
         documentroot_group => $group, 
@@ -104,7 +104,81 @@ define webhosting::modperl(
         nagios_check_code => $nagios_check_code,
     }
     apache::vhost::modperl{"${name}":
-        domainalias => $real_domainalias,
+        domainalias => $domainalias,
+        group => $group,
+        allow_override => $allow_override,
+        do_includes => $do_includes,
+        options => $options,
+        additional_options => $additional_options,
+        default_charset => $default_charset,
+        run_mode => $run_mode,
+        run_uid => "${name}_run",
+        run_gid => "${name}",
+        ssl_mode => $ssl_mode,
+        vhost_mode => $vhost_mode,
+        vhost_source => $vhost_source,
+        vhost_destination => $vhost_destination,
+        htpasswd_file => $htpasswd_file,
+        require => [ User::Sftp_only["${name}"], User::Managed["${name}_run"] ],
+    }
+    case $run_mode {
+        'itk': {
+            Apache::Vhost::Modperl[$name]{
+                documentroot_owner => $name,
+                documentroot_group => $name,
+                documentroot_mode => 0750,
+            }
+        }
+    } 
+}
+
+# run_mode: 
+#   - normal: nothing special (*default*)
+#   - itk: apache is running with the itk module 
+#          and run_uid and run_gid are used as vhost users
+# run_uid: the uid the vhost should run as with the itk module
+# run_gid: the gid the vhost should run as with the itk module
+define webhosting::php(
+    $uid = 'absent',
+    $gid = 'uid',
+    $password = 'absent',
+    $password_crypted = true,
+    $domainalias = 'www',
+    $owner = root,
+    $group = 'sftponly',
+    $run_mode = 'normal',
+    $run_uid = 'absent',
+    $run_gid = 'absent',
+    $allow_override = 'None',
+    $do_includes = false,
+    $options = 'absent',
+    $additional_options = 'absent',
+    $default_charset = 'absent',
+    $ssl_mode = false,
+    $vhost_mode = 'template',
+    $vhost_source = 'absent',
+    $vhost_destination = 'absent',
+    $htpasswd_file = 'absent',
+    $nagios_check_domain = 'absent',
+    $nagios_check_url = '/',
+    $nagios_check_code = 'OK'
+){
+    webhosting::common{$name:
+        uid => $uid,
+        gid => $gid,
+        password => $password,
+        password_crypted => $password_crypted,
+        htpasswd_file => $htpasswd_file,
+        ssl_mode => $ssl_mode,
+        run_mode => $run_mode,
+        run_uid => $run_uid,
+        run_gid => $run_gid,
+        nagios_check_domain => $nagios_check_domain,
+        nagios_check_url => $nagios_check_url,
+        nagios_check_code => $nagios_check_code,
+    }
+    apache::vhost::php::standard{"${name}":
+        domainalias => $domainalias,
         group => $group,
         allow_override => $allow_override,
         do_includes => $do_includes,
