df71a02d8a89208609e53d44f5de7fb5f7d5b222
andreas <andreas@immerda.ch>
2009-01-27 16:35:12 +0000

    extending apache and webhosting modules for joomla. extending git module with clone

--- manifests/defines/webhosting_varieties.pp
+++ manifests/defines/webhosting_varieties.pp
@@ -215,3 +215,83 @@ define webhosting::php(
         }
     } 
 }
+
+# run_mode: 
+#   - normal: nothing special (*default*)
+#   - itk: apache is running with the itk module 
+#          and run_uid and run_gid are used as vhost users
+# run_uid: the uid the vhost should run as with the itk module
+# run_gid: the gid the vhost should run as with the itk module
+define webhosting::php::joomla(
+    $uid = 'absent',
+    $gid = 'uid',
+    $password = 'absent',
+    $password_crypted = true,
+    $domainalias = 'www',
+    $owner = root,
+    $group = 'sftponly',
+    $run_mode = 'normal',
+    $run_uid = 'absent',
+    $run_gid = 'absent',
+    $allow_override = 'None',
+    $do_includes = false,
+    $options = 'absent',
+    $additional_options = 'absent',
+    $default_charset = 'absent',
+    $ssl_mode = false,
+    $vhost_mode = 'template',
+    $vhost_source = 'absent',
+    $vhost_destination = 'absent',
+    $htpasswd_file = 'absent',
+    $nagios_check_domain = 'absent',
+    $nagios_check_url = '/',
+    $nagios_check_code = 'OK'
+){
+    webhosting::common{$name:
+        uid => $uid,
+        gid => $gid,
+        password => $password,
+        password_crypted => $password_crypted,
+        htpasswd_file => $htpasswd_file,
+        ssl_mode => $ssl_mode,
+        run_mode => $run_mode,
+        run_uid => $run_uid,
+        run_gid => $run_gid,
+        nagios_check_domain => $nagios_check_domain,
+        nagios_check_url => $nagios_check_url,
+        nagios_check_code => $nagios_check_code,
+    }
+	apache::vhost::php::joomla{"${name}":
+        domainalias => $domainalias,
+        group => $group,
+        allow_override => $allow_override,
+        do_includes => $do_includes,
+        options => $options,
+        additional_options => $additional_options,
+        default_charset => $default_charset,
+        run_mode => $run_mode,
+        ssl_mode => $ssl_mode,
+        vhost_mode => $vhost_mode,
+        vhost_source => $vhost_source,
+        vhost_destination => $vhost_destination,
+        htpasswd_file => $htpasswd_file,
+    }
+    case $run_mode {
+        'itk': {
+            Apache::Vhost::Php::Joomla[$name]{
+                documentroot_owner => $name,
+                documentroot_group => $name,
+                documentroot_mode => 0750,
+                run_uid => "${name}_run",
+                run_gid => "${name}",
+                require => [ User::Sftp_only["${name}"], User::Managed["${name}_run"] ],
+            }
+        }
+        default: {
+            Apache::Vhost::Php::Joomla[$name]{
+                require => User::Sftp_only["${name}"], 
+            }
+        }
+    } 
+}
+
