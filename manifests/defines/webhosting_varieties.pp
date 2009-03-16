34907d0528b9ab7f760c66ddde1ca743a75e1cad
mh <mh@immerda.ch>
2009-03-16 00:11:34 +0000

    added simpelmachine hosting

--- manifests/defines/webhosting_varieties.pp
+++ manifests/defines/webhosting_varieties.pp
@@ -495,3 +495,133 @@ define webhosting::php::wordpress(
     }
 }
 
+# run_mode:
+#   - normal: nothing special (*default*)
+#   - itk: apache is running with the itk module
+#          and run_uid and run_gid are used as vhost users
+# run_uid: the uid the vhost should run as with the itk module
+# run_gid: the gid the vhost should run as with the itk module
+define webhosting::php::simplemachine(
+    $ensure = present,
+    $uid = 'absent',
+    $gid = 'uid',
+    $password = 'absent',
+    $password_crypted = true,
+    $domainalias = 'www',
+    $owner = root,
+    $group = 'sftponly',
+    $run_mode = 'normal',
+    $run_uid = 'absent',
+    $run_gid = 'absent',
+    $allow_override = 'FileInfo',
+    $do_includes = false,
+    $options = 'absent',
+    $additional_options = 'absent',
+    $default_charset = 'absent',
+    $ssl_mode = false,
+    $vhost_mode = 'template',
+    $vhost_source = 'absent',
+    $vhost_destination = 'absent',
+    $htpasswd_file = 'absent',
+    $nagios_check = 'ensure',
+    $nagios_check_domain = 'absent',
+    $nagios_check_url = '/',
+    $nagios_check_code = 'OK',
+    $git_repo = 'absent',
+    $mod_security = true,
+    $manage_config = true,
+    $config_webwriteable = false,
+    $manage_directories = true
+){
+    webhosting::common{$name:
+        ensure => $ensure,
+        uid => $uid,
+        gid => $gid,
+        password => $password,
+        password_crypted => $password_crypted,
+        htpasswd_file => $htpasswd_file,
+        ssl_mode => $ssl_mode,
+        run_mode => $run_mode,
+        run_uid => $run_uid,
+        run_gid => $run_gid,
+        nagios_check => $nagios_check,
+        nagios_check_domain => $nagios_check_domain,
+        nagios_check_url => $nagios_check_url,
+        nagios_check_code => $nagios_check_code,
+    }
+
+    $path = $operatingsystem ? {
+        openbsd => "/var/www/htdocs/${name}",
+        default => "/var/www/vhosts/${name}"
+    }
+    $documentroot = "${path}/www"
+
+    apache::vhost::php::simplemachine{"${name}":
+        ensure => $ensure,
+        domainalias => $domainalias,
+        group => $group,
+        allow_override => $allow_override,
+        do_includes => $do_includes,
+        options => $options,
+        additional_options => $additional_options,
+        default_charset => $default_charset,
+        run_mode => $run_mode,
+        ssl_mode => $ssl_mode,
+        vhost_mode => $vhost_mode,
+        vhost_source => $vhost_source,
+        vhost_destination => $vhost_destination,
+        htpasswd_file => $htpasswd_file,
+        mod_security => $mod_security,
+        manage_config => $manage_config,
+        config_webwriteable => $config_webwriteable,
+        manage_directories => $manage_directories,
+    }
+    if $git_repo != 'absent' {
+        # create webdir
+        # for the cloning, $documentroot needs to be absent
+        git::clone{"git_clone_$name":
+            ensure => $ensure,
+            git_repo => $git_repo,
+            projectroot => $documentroot,
+            cloneddir_user => $name,
+            cloneddir_group => $name,
+            before =>  Apache::Vhost::Php::Wordpress[$name],
+        }
+        apache::vhost::file::documentrootdir{"simplemachinesgitdir_${name}":
+            documentroot => $documentroot,
+            filename => '.git',
+            thedomain => $name,
+            owner => $name,
+            group => 'root',
+            mode => 400,
+        }
+    }
+    case $run_mode {
+        'itk': {
+            Apache::Vhost::Php::Simplemachine[$name]{
+                documentroot_owner => $name,
+                documentroot_group => $name,
+                documentroot_mode => 0750,
+                run_uid => "${name}_run",
+                run_gid => "${name}",
+                require => [ User::Sftp_only["${name}"], User::Managed["${name}_run"] ],
+            }
+            if $git_repo != 'absent' {
+                Git::Clone["git_clone_$name"]{
+                    require => [ User::Sftp_only["${name}"], User::Managed["${name}_run"] ],
+                }
+            }
+        }
+        default: {
+            Apache::Vhost::Php::Simplemachine[$name]{
+                require => User::Sftp_only["${name}"],
+            }
+            if $git_repo != 'absent' {
+                Git::Clone["git_clone_$name"]{
+                    require => User::Sftp_only["${name}"],
+                }
+            }
+        }
+    }
+}
+
