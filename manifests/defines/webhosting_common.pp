e5ec9f32dce208d6b5b802f8fa4798e84c56ecb3
mh <mh@immerda.ch>
2009-04-30 09:44:45 +0000

    cleaned up the webhosting::common logic, if ensure is absent you don't have to set anymore a uid and/or gid

--- manifests/defines/webhosting_common.pp
+++ manifests/defines/webhosting_common.pp
@@ -17,67 +17,65 @@ define webhosting::common(
     $nagios_check_code = 'OK'
 ){
     user::sftp_only{"${name}":
-        ensure => $ensure,
-        uid => $uid,
-        gid => $gid,
-        password => $password,
-        password_crypted => $password_crypted,
+      ensure => $ensure,
+      uid => $uid,
+      gid => $gid,
+      password => $password,
+      password_crypted => $password_crypted,
     }
 
     if $use_nagios {
-        case $nagios_check {
-            'ensure': { $nagios_ensure = $ensure }
-            default: { $nagios_ensure = $nagios_check }
+      if $nagios_check == 'ensure' {
+        $nagios_ensure = $ensure
+      } else {
+        $nagios_ensure = $nagios_check
+      }
+      if $nagios_check_code == 'OK' {
+        $real_nagios_check_code = $htpasswd_file ? {
+          'absent' => $nagios_check_code,
+          default => '401'
         }
-        case $nagios_check_code {
-            'OK': {
-                    $real_nagios_check_code = $htpasswd_file ? {
-                        'absent' => $nagios_check_code,
-                        default => '401'
-                    }
-            }
-            default: { $real_nagios_check_code = $nagios_check_code }
-        }
+      } else {
+        $real_nagios_check_code = $nagios_check_code
+      }
 
-        nagios::service::http{"${name}":
-            ensure => $nagios_ensure,
-            check_domain => $nagios_check_domain,
-            ssl_mode => $ssl_mode,
-            check_url => $nagios_check_url,
-            check_code => $real_nagios_check_code,
-        }
+      nagios::service::http{"${name}":
+        ensure => $nagios_ensure,
+        check_domain => $nagios_check_domain,
+        ssl_mode => $ssl_mode,
+        check_url => $nagios_check_url,
+        check_code => $real_nagios_check_code,
+      }
     }
 
-   case $run_mode {
-        'itk': {
-            case $run_uid {
-                'absent': { fail("you need to define run_uid for $name on $fqdn to use itk") }
-            }
-            case $run_gid {
-                'absent': {
-                    case $gid {
-                        'uid': { $real_run_gid = $uid }
-                        default: { $real_run_gid = $gid }
-                    }
-                }
-                default: { $real_run_gid = $run_gid }
-            }
-            user::managed{"${name}_run":
-                ensure => $ensure,
-                uid => $run_uid,
-                gid => $real_run_gid,
-                manage_group => false,
-                managehome => false,
-                shell => $operatingsystem ? {
-                    debian => '/usr/sbin/nologin',
-                    ubuntu => '/usr/sbin/nologin',
-                    default => '/sbin/nologin'
-                },
-                require => User::Sftp_only[$name],
-            }
-            User::Sftp_only["${name}"]{
-                homedir_mode => 0755
-            }
+    if ($run_mode == 'itk') {
+      if ($run_uid=='absent') and ($ensure != 'absent') {
+        fail("you need to define run_uid for $name on $fqdn to use itk")
+      }
+      if ($run_gid == 'absent') {
+        if ($gid == 'uid') {
+          $real_run_gid = $uid
+        } else {
+          $real_run_gid = $gid
         }
-    }
+      } else {
+        $real_run_gid = $run_gid
+      }
+      user::managed{"${name}_run":
+        ensure => $ensure,
+        uid => $run_uid,
+        gid => $real_run_gid,
+        manage_group => false,
+        managehome => false,
+        shell => $operatingsystem ? {
+          debian => '/usr/sbin/nologin',
+          ubuntu => '/usr/sbin/nologin',
+          default => '/sbin/nologin'
+        },
+        require => User::Sftp_only[$name],
+      }
+      User::Sftp_only["${name}"]{
+        homedir_mode => 0755
+      }
+   }
 }
