7d953a49949cc4b9c4b4a592fcef43ea1da4619a
mh <mh@immerda.ch>
2009-04-30 18:40:05 +0000

    fix typo

--- manifests/defines/webhosting_varieties.pp
+++ manifests/defines/webhosting_varieties.pp
@@ -689,7 +689,7 @@ define webhosting::php::simplemachine(
         documentroot_group => $name,
         documentroot_mode => 0750,
         run_uid => $real_run_uid_name,
-        run_gid => real_run_gid_name,
+        run_gid => $real_run_gid_name,
         require => [ User::Sftp_only["${name}"], User::Managed["${real_run_uid_name}"] ],
       }
       if ($git_repo != 'absent') and ($ensure != 'absent') {
