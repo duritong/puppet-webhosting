317359aad1d9b7a04a3d06c17090a497f03224b5
mh <mh@immerda.ch>
2010-04-23 02:18:34 +0200

    fix missing part :/

--- manifests/php.pp
+++ manifests/php.pp
@@ -107,7 +107,6 @@ define webhosting::php(
         run_uid => $run_uid,
         run_uid_name => $run_uid_name,
         run_gid => $run_gid,
-        run_gid_name => $run_gid_name,
         nagios_check => $nagios_check,
         nagios_check_domain => $nagios_check_domain,
         nagios_check_url => $nagios_check_url,
@@ -145,7 +144,10 @@ define webhosting::php(
                 $real_run_uid_name = $run_uid_name
             }
             if ($run_gid_name == 'absent'){
-                $real_run_gid_name = $name
+              $real_run_gid_name = $gid_name ? {
+                'absent' => $name,
+                default => $gid_name
+              }
             } else {
                 $real_run_gid_name = $run_gid_name
             }
