3334bd45c4fe10ebef952b870b3534f2878fb598
mh <mh@immerda.ch>
2012-05-10 21:22:13 +0200

    fix typo

--- manifests/watch_adjust_webfiles/base.pp
+++ manifests/watch_adjust_webfiles/base.pp
@@ -1,6 +1,6 @@
 class webhosting::watch_adjust_webfiles::base {
   file{'/usr/local/sbin/chown_webfiles.sh':
     source => 'puppet:///modules/webhosting/watch_adjust_webfiles/chown_webfiles.sh',
-    onwer => root, group => 0, mode => 0700;
+    owner => root, group => 0, mode => 0700;
   }
 }
