ea9def7c678b357fbdfdd86371a160749b5d8d7f
mh <mh@immerda.ch>
2012-06-26 15:08:13 -0400

    fix path

--- files/user_scripts/common/run_incron.sh
+++ files/user_scripts/common/run_incron.sh
@@ -9,7 +9,7 @@ script_basename=`basename $2 .run`
 [ "`basename $1`" != "${script_basename}" ] && exit 1
 
 # is it an existing script?
-script="/opt/webscripts/${script_basename}/${script_basename}.rb"
+script="/opt/webhosting_user_scripts/${script_basename}/${script_basename}.rb"
 [ -x ${script} ] || exit 1
 
 $script $1/$2 >> $1/${script_basename}.log
