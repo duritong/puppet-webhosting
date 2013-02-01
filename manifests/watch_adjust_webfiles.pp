870f57bc9f70041ae0c95c4b3a99ae98569c056b
mh <mh@immerda.ch>
2013-02-01 13:22:00 +0100

    do webfiles only with daily cron and not with inotifies by default as it will use too much memory

--- manifests/watch_adjust_webfiles.pp
+++ manifests/watch_adjust_webfiles.pp
@@ -1,22 +1,53 @@
+# add a adjust_watch for webfiles
+#
+#  ensure:
+#    'present': cron
+#    'both': cron & inotify
+#    'inotify': inotify
+#    'absent': absent
 define webhosting::watch_adjust_webfiles (
   $ensure = 'present',
   $path,
   $sftp_user,
   $run_user
 ) {
-  require webhosting::watch_adjust_webfiles::base
-  dirwatcher::job {
-    $name :
-      ensure => $ensure,
-      watch_directory => "${path}",
-      watch_events => 'create,move_to',
-      watch_command => "/usr/local/sbin/chown_webfiles.sh ${run_user} ${sftp_user} ${path} \$filename",
+
+  $file_ensure = $ensure ? {
+    'absent'  => 'absent',
+    'inotify' => 'absent',
+    default   => 'present'
   }
-  file{
-    "/etc/cron.daily/fix_webperms_${name}":
-      ensure => $ensure,
+
+  $job_ensure = $ensure ? {
+    'both'    => 'present',
+    'inotify' => 'present',
+    default   => 'absent'
+  }
+
+  file{"/etc/cron.daily/fix_webperms_${name}":
+    ensure => $file_ensure,
+  }
+
+  dirwatcher::job {$name :
+    ensure => $job_ensure,
+  }
+
+  if $job_ensure == 'present' {
+    require webhosting::watch_adjust_webfiles::base
+    Dirwatcher::Job[$name]{
+      watch_directory => $path,
+      watch_events    => 'create,move_to',
+      watch_command   => "/usr/local/sbin/chown_webfiles.sh ${run_user} ${sftp_user} ${path} \$filename",
+    }
+  }
+
+  if $file_ensure == 'present' {
+    File["/etc/cron.daily/fix_webperms_${name}"]{
       content => "#!/bin/env bash\nfind ${path} -user ${run_user} -exec /usr/local/sbin/chown_webfiles.sh ${run_user} ${sftp_user} '{}' \\;\n",
-      owner => root, group => 0, mode => 0700;
+      owner   => root,
+      group   => 0,
+      mode    => 0700,
+    }
   }
 }
 
