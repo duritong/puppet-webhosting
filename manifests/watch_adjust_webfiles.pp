1e550881e227fb071efe14b841ad6964a108a8e1
o <o@immerda.ch>
2013-08-25 15:16:05 +0200

    Prevent find from complaining about deleted files

--- manifests/watch_adjust_webfiles.pp
+++ manifests/watch_adjust_webfiles.pp
@@ -32,17 +32,25 @@ define webhosting::watch_adjust_webfiles (
     require webhosting::watch_adjust_webfiles::base
   }
 
+  $chown_script = "/usr/local/sbin/chown_webfiles.sh"
+
   if $job_ensure == 'present' {
+    $watch_cmd = "${chown_script} ${run_user} ${sftp_user} ${path} \$filename"
+
     dirwatcher::job {$name :
       watch_directory => $path,
       watch_events    => 'create,move_to',
-      watch_command   => "/usr/local/sbin/chown_webfiles.sh ${run_user} ${sftp_user} ${path} \$filename",
+      watch_command   => $watch_cmd,
     }
   }
 
   if $file_ensure == 'present' {
+    $cron_cmd = "#!/bin/env bash
+find ${path} -ignore_readdir_race -user ${run_user} -exec ${chown_script}\\ 
+${run_user} ${sftp_user} ${path} '{}' \\;"
+
     File["/etc/cron.daily/fix_webperms_${name}"]{
-      content => "#!/bin/env bash\nfind ${path} -user ${run_user} -exec /usr/local/sbin/chown_webfiles.sh ${run_user} ${sftp_user} ${path} '{}' \\;\n",
+      content => $cron_cmd,
       owner   => root,
       group   => 0,
       mode    => 0700,
