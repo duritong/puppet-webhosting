1e15dfa07d31a839346205d3f80aee2fc88eb8c4
mh <mh@immerda.ch>
2013-11-02 15:44:17 +0100

    fix urls

--- manifests/php/simplemachine/base.pp
+++ manifests/php/simplemachine/base.pp
@@ -3,7 +3,7 @@
 class webhosting::php::simplemachine::base {
   file{
     '/usr/local/sbin/update_smf_webhostings.sh':
-      source => 'puppet:///modules/webhostings/update_scripts/update_smfs_webhostings.sh',
+      source => 'puppet:///modules/webhosting/update_scripts/update_smfs_webhostings.sh',
       owner  => root,
       group  => 0,
       mode   => '0700';
