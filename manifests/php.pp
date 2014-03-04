349d5705520e5860308e04fd4e3318d65f9873a3
mh <mh@immerda.ch>
2014-03-04 20:05:38 +0100

    linting

--- manifests/php.pp
+++ manifests/php.pp
@@ -27,161 +27,161 @@
 #   - anonym: Don't log ips for CustomLog, send ErrorLog to /dev/null
 #   - semianonym: Don't log ips for CustomLog, log normal ErrorLog
 define webhosting::php(
-    $ensure = present,
-    $uid = 'absent',
-    $uid_name = 'absent',
-    $gid = 'uid',
-    $gid_name = 'absent',
-    $user_provider = 'local',
-    $user_access = 'sftp',
-    $webdav_domain = 'absent',
-    $webdav_ssl_mode = false,
-    $password = 'absent',
-    $password_crypted = true,
-    $domain = 'absent',
-    $domainalias = 'www',
-    $server_admin = 'absent',
-    $logmode = 'default',
-    $owner = root,
-    $group = 'absent',
-    $run_mode = 'normal',
-    $run_uid = 'absent',
-    $run_uid_name = 'absent',
-    $run_gid = 'absent',
-    $run_gid_name = 'absent',
-    $wwwmail = false,
-    $watch_adjust_webfiles = 'absent',
-    $user_scripts = 'absent',
-    $user_scripts_options = {},
-    $allow_override = 'None',
-    $do_includes = false,
-    $options = 'absent',
-    $additional_options = 'absent',
-    $default_charset = 'absent',
-    $php_settings = {},
-    $php_options = {},
-    $ssl_mode = false,
-    $vhost_mode = 'template',
-    $template_partial = 'absent',
-    $vhost_source = 'absent',
-    $vhost_destination = 'absent',
-    $htpasswd_file = 'absent',
-    $nagios_check = 'ensure',
-    $nagios_check_domain = 'absent',
-    $nagios_check_url = '/',
-    $nagios_check_code = '200',
-    $nagios_use = 'generic-service',
-    $mod_security = true,
-    $ldap_user = 'absent'
+  $ensure                 = present,
+  $uid                    = 'absent',
+  $uid_name               = 'absent',
+  $gid                    = 'uid',
+  $gid_name               = 'absent',
+  $user_provider          = 'local',
+  $user_access            = 'sftp',
+  $webdav_domain          = 'absent',
+  $webdav_ssl_mode        = false,
+  $password               = 'absent',
+  $password_crypted       = true,
+  $domain                 = 'absent',
+  $domainalias            = 'www',
+  $server_admin           = 'absent',
+  $logmode                = 'default',
+  $owner                  = root,
+  $group                  = 'absent',
+  $run_mode               = 'normal',
+  $run_uid                = 'absent',
+  $run_uid_name           = 'absent',
+  $run_gid                = 'absent',
+  $run_gid_name           = 'absent',
+  $wwwmail                = false,
+  $watch_adjust_webfiles  = 'absent',
+  $user_scripts           = 'absent',
+  $user_scripts_options   = {},
+  $allow_override         = 'None',
+  $do_includes            = false,
+  $options                = 'absent',
+  $additional_options     = 'absent',
+  $default_charset        = 'absent',
+  $php_settings           = {},
+  $php_options            = {},
+  $ssl_mode               = false,
+  $vhost_mode             = 'template',
+  $template_partial       = 'absent',
+  $vhost_source           = 'absent',
+  $vhost_destination      = 'absent',
+  $htpasswd_file          = 'absent',
+  $nagios_check           = 'ensure',
+  $nagios_check_domain    = 'absent',
+  $nagios_check_url       = '/',
+  $nagios_check_code      = '200',
+  $nagios_use             = 'generic-service',
+  $mod_security           = true,
+  $ldap_user              = 'absent'
 ){
 
-    if ($group == 'absent') and ($user_access == 'sftp') {
-        $real_group = 'sftponly'
-    } else {
-        if ($group == 'absent') {
-            $real_group = 'apache'
-        } else {
-            $real_group = $group
-        }
-    }
-    if ($uid_name == 'absent'){
-      $real_uid_name = $name
-    } else {
-      $real_uid_name = $uid_name
-    }
-    if ($gid_name == 'absent'){
-      $real_gid_name = $real_uid_name
-    } else {
-      $real_gid_name = $gid_name
-    }
-    webhosting::common{$name:
-        ensure => $ensure,
-        uid => $uid,
-        uid_name => $real_uid_name,
-        gid => $gid,
-        gid_name => $real_gid_name,
-        user_provider => $user_provider,
-        user_access => $user_access,
-        webdav_domain => $webdav_domain,
-        webdav_ssl_mode => $webdav_ssl_mode,
-        password => $password,
-        password_crypted => $password_crypted,
-        htpasswd_file => $htpasswd_file,
-        ssl_mode => $ssl_mode,
-        run_mode => $run_mode,
-        run_uid => $run_uid,
-        run_uid_name => $run_uid_name,
-        run_gid => $run_gid,
-        watch_adjust_webfiles => $watch_adjust_webfiles,
-        user_scripts => $user_scripts,
-        user_scripts_options => $user_scripts_options,
-        wwwmail => $wwwmail,
-        nagios_check => $nagios_check,
-        nagios_check_domain => $nagios_check_domain,
-        nagios_check_url => $nagios_check_url,
-        nagios_check_code => $nagios_check_code,
-        nagios_use => $nagios_use,
-        ldap_user => $ldap_user,
-    }
-    apache::vhost::php::standard{"${name}":
-        ensure => $ensure,
-        domain => $domain,
-        domainalias => $domainalias,
-        server_admin => $server_admin,
-        logmode => $logmode,
-        group => $real_group,
-        allow_override => $allow_override,
-        do_includes => $do_includes,
-        options => $options,
-        additional_options => $additional_options,
-        default_charset => $default_charset,
-        php_settings => $php_settings,
-        php_options => $php_options,
-        run_mode => $run_mode,
-        ssl_mode => $ssl_mode,
-        vhost_mode => $vhost_mode,
-        vhost_source => $vhost_source,
-        vhost_destination => $vhost_destination,
-        htpasswd_file => $htpasswd_file,
-        mod_security => $mod_security,
-    }
-    case $run_mode {
-        'fcgid','itk','proxy-itk','static-itk': {
-            if ($run_uid_name == 'absent'){
-                $real_run_uid_name = "${name}_run"
-            } else {
-                $real_run_uid_name = $run_uid_name
-            }
-            if ($run_gid_name == 'absent'){
-              $real_run_gid_name = $real_gid_name
-            } else {
-                $real_run_gid_name = $run_gid_name
-            }
-            Apache::Vhost::Php::Standard[$name]{
-              documentroot_owner => $real_uid_name,
-              documentroot_group => $real_gid_name,
-              documentroot_mode => 0750,
-              run_uid => $real_run_uid_name,
-              run_gid => $real_run_gid_name,
-            }
-            if ($user_provider == 'local') {
-                Apache::Vhost::Php::Standard[$name]{
-                  require => [ User::Sftp_only["${real_uid_name}"], User::Managed["${real_run_uid_name}"] ],
-                }
-            }
-        }
-        default: {
-            if ($user_provider == 'local') {
-                Apache::Vhost::Php::Standard[$name]{
-                    require => User::Sftp_only["${real_uid_name}"],
-                }
-            }
-        }
-    }
-    if $template_partial != 'absent' {
+  if ($group == 'absent') and ($user_access == 'sftp') {
+      $real_group = 'sftponly'
+  } else {
+      if ($group == 'absent') {
+          $real_group = 'apache'
+      } else {
+          $real_group = $group
+      }
+  }
+  if ($uid_name == 'absent'){
+    $real_uid_name = $name
+  } else {
+    $real_uid_name = $uid_name
+  }
+  if ($gid_name == 'absent'){
+    $real_gid_name = $real_uid_name
+  } else {
+    $real_gid_name = $gid_name
+  }
+  webhosting::common{$name:
+    ensure                => $ensure,
+    uid                   => $uid,
+    uid_name              => $real_uid_name,
+    gid                   => $gid,
+    gid_name              => $real_gid_name,
+    user_provider         => $user_provider,
+    user_access           => $user_access,
+    webdav_domain         => $webdav_domain,
+    webdav_ssl_mode       => $webdav_ssl_mode,
+    password              => $password,
+    password_crypted      => $password_crypted,
+    htpasswd_file         => $htpasswd_file,
+    ssl_mode              => $ssl_mode,
+    run_mode              => $run_mode,
+    run_uid               => $run_uid,
+    run_uid_name          => $run_uid_name,
+    run_gid               => $run_gid,
+    watch_adjust_webfiles => $watch_adjust_webfiles,
+    user_scripts          => $user_scripts,
+    user_scripts_options  => $user_scripts_options,
+    wwwmail               => $wwwmail,
+    nagios_check          => $nagios_check,
+    nagios_check_domain   => $nagios_check_domain,
+    nagios_check_url      => $nagios_check_url,
+    nagios_check_code     => $nagios_check_code,
+    nagios_use            => $nagios_use,
+    ldap_user             => $ldap_user,
+  }
+  apache::vhost::php::standard{$name:
+    ensure              => $ensure,
+    domain              => $domain,
+    domainalias         => $domainalias,
+    server_admin        => $server_admin,
+    logmode             => $logmode,
+    group               => $real_group,
+    allow_override      => $allow_override,
+    do_includes         => $do_includes,
+    options             => $options,
+    additional_options  => $additional_options,
+    default_charset     => $default_charset,
+    php_settings        => $php_settings,
+    php_options         => $php_options,
+    run_mode            => $run_mode,
+    ssl_mode            => $ssl_mode,
+    vhost_mode          => $vhost_mode,
+    vhost_source        => $vhost_source,
+    vhost_destination   => $vhost_destination,
+    htpasswd_file       => $htpasswd_file,
+    mod_security        => $mod_security,
+  }
+  case $run_mode {
+    'fcgid','itk','proxy-itk','static-itk': {
+      if ($run_uid_name == 'absent'){
+        $real_run_uid_name = "${name}_run"
+      } else {
+        $real_run_uid_name = $run_uid_name
+      }
+      if ($run_gid_name == 'absent'){
+        $real_run_gid_name = $real_gid_name
+      } else {
+        $real_run_gid_name = $run_gid_name
+      }
       Apache::Vhost::Php::Standard[$name]{
-        template_partial => $template_partial
+        documentroot_owner  => $real_uid_name,
+        documentroot_group  => $real_gid_name,
+        documentroot_mode   => '0750',
+        run_uid             => $real_run_uid_name,
+        run_gid             => $real_run_gid_name,
+      }
+      if ($user_provider == 'local') {
+        Apache::Vhost::Php::Standard[$name]{
+          require => [ User::Sftp_only[$real_uid_name], User::Managed[$real_run_uid_name] ],
+        }
       }
     }
+    default: {
+      if ($user_provider == 'local') {
+        Apache::Vhost::Php::Standard[$name]{
+          require => User::Sftp_only[$real_uid_name],
+        }
+      }
+    }
+  }
+  if $template_partial != 'absent' {
+    Apache::Vhost::Php::Standard[$name]{
+      template_partial => $template_partial
+    }
+  }
 }
 
