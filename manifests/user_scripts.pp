8c24ee7595b603669f82e74f7bcd12ed72d2a9d1
mh <mh@immerda.ch>
2017-09-29 20:08:13 +0200

    use a define to streamline how the scripts are deployed

--- manifests/user_scripts.pp
+++ manifests/user_scripts.pp
@@ -24,48 +24,14 @@ class webhosting::user_scripts {
   }
 
   # script to adjust permission in web directories
-  file{
-    '/opt/webhosting_user_scripts/adjust_permissions':
-      ensure => directory,
-      owner  => root,
-      group  => 0,
-      mode   => '0400';
-    '/opt/webhosting_user_scripts/adjust_permissions/adjust_permissions.rb':
-      source => 'puppet:///modules/webhosting/user_scripts/adjust_permissions/adjust_permissions.rb',
-      owner  => root,
-      group  => 0,
-      mode   => '0500';
-  }
+  webhosting::user_scripts::script{'adjust_permissions': }
 
   # update mode script
   include ::acl::requirements
-  file{
-    '/opt/webhosting_user_scripts/update_mode':
-      ensure => directory,
-      owner  => root,
-      group  => 0,
-      mode   => '0400';
-    '/opt/webhosting_user_scripts/update_mode/update_mode.rb':
-      source => 'puppet:///modules/webhosting/user_scripts/update_mode/update_mode.rb',
-      owner  => root,
-      group  => 0,
-      mode   => '0500';
-  }
+  webhosting::user_scripts::script{'update_mode': }
 
-  if versioncmp($::operatingsystemmajrelease,'5') > 0 {
-    require ::wordpress::base
-    require ::tmpwatch
-    file{
-      '/opt/webhosting_user_scripts/update_wordpress':
-        ensure => directory,
-        owner  => root,
-        group  => 0,
-        mode   => '0400';
-      '/opt/webhosting_user_scripts/update_wordpress/update_wordpress.rb':
-        source => 'puppet:///modules/webhosting/user_scripts/update_wordpress/update_wordpress.rb',
-        owner  => root,
-        group  => 0,
-        mode   => '0500';
-    }
-  }
+  # wordpress updates
+  require ::wordpress::base
+  require ::tmpwatch
+  webhosting::user_scripts::script{'update_wordpress': }
 }
