1d22cd62089d03349678483a102e307e8f294e7e
mh <mh@immerda.ch>
2018-10-17 14:10:45 +0200

    enable php 7.2 for mediawiki

--- manifests/php/mediawiki.pp
+++ manifests/php/mediawiki.pp
@@ -40,7 +40,7 @@ define webhosting::php::mediawiki(
   $ssl_mode            = false,
   $php_settings        = {},
   $php_options         = {},
-  $php_installation    = 'scl56',
+  $php_installation    = 'scl72',
   $vhost_mode          = 'template',
   $template_partial    = 'absent',
   $vhost_source        = 'absent',
