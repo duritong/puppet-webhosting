b15acc02d6922da8ce14666e056d4c0199f4cd9e
mh <mh@immerda.ch>
2021-10-03 18:59:41 +0200

    enable additional type

--- types/userfiles.pp
+++ types/userfiles.pp
@@ -1,12 +1,13 @@
 type Webhosting::Userfiles = Hash[
   Variant[Stdlib::Unixpath, String[1]],
   Struct[{
-      content    => Optional[String],
-      source     => Optional[String],
-      ensure     => Optional[Enum['directory','file']],
-      replace    => Optional[Boolean],
-      owner      => Optional[Variant[String,Integer]],
-      mode       => Optional[Stdlib::Filemode],
-      ensure_acl => Optional[Boolean],
+      content                 => Optional[String],
+      source                  => Optional[String],
+      ensure                  => Optional[Enum['directory','file']],
+      replace                 => Optional[Boolean],
+      owner                   => Optional[Variant[String,Integer]],
+      mode                    => Optional[Stdlib::Filemode],
+      selinux_ignore_defaults => Optional[Boolean],
+      ensure_acl              => Optional[Boolean],
   }]
 ]
