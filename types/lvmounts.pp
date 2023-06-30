fda93c18fd2f10e5eb72a64ddcb68d7cb973fcca
mh <mh@immerda.ch>
2023-06-30 10:03:53 +0200

    support selinux_ignore_defaults

--- types/lvmounts.pp
+++ types/lvmounts.pp
@@ -1,16 +1,17 @@
 type Webhosting::Lvmounts = Hash[
   Pattern[/^[a-zA-Z0-9_\-]+$/],
   Struct[{
-      ensure        => Optional[Enum['present','absent']],
-      folder        => String[1],
-      size          => Pattern[/^[0-9]+(G|M)$/],
-      owner         => Optional[String[1]],
-      group         => Optional[String[1]],
-      mode          => Optional[Stdlib::Filemode],
-      seltype       => Optional[String[1]],
-      manage_folder => Optional[Boolean],
-      mount_options => Optional[String[1]],
-      fs_type       => Optional[Enum['ext4','xfs']],
-      fs_options    => Optional[String[1]],
+      ensure                  => Optional[Enum['present','absent']],
+      folder                  => String[1],
+      size                    => Pattern[/^[0-9]+(G|M)$/],
+      owner                   => Optional[String[1]],
+      group                   => Optional[String[1]],
+      mode                    => Optional[Stdlib::Filemode],
+      seltype                 => Optional[String[1]],
+      selinux_ignore_defaults => Optional[Boolean],
+      manage_folder           => Optional[Boolean],
+      mount_options           => Optional[String[1]],
+      fs_type                 => Optional[Enum['ext4','xfs']],
+      fs_options              => Optional[String[1]],
   }]
 ]
