941cdfc748806de527e6c66295fcae3bc2744d22
mh <mh@immerda.ch>
2023-12-02 16:01:44 +0100

    these params are optional

--- types/cronjobs.pp
+++ types/cronjobs.pp
@@ -1,10 +1,10 @@
 type Webhosting::Cronjobs = Hash[
   Pattern[/^[a-zA-Z0-9_\-]+$/],
   Struct[{
-      ensure                 => Optional[Enum['present','absent']],
       cmd                    => String[1],
-      group                  => String[1],
-      supplementary_groups   => Array[String[1]],
+      ensure                 => Optional[Enum['present','absent']],
+      group                  => Optional[String[1]],
+      supplementary_groups   => Optional[Array[String[1]]],
       on_calendar            => Optional[String],
       randomize_delay_sec    => Optional[String],
       read_write_directories => Optional[Array[Stdlib::Unixpath]],
